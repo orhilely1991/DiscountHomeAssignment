6bb320f2df41cdb982224366056012b0f3dccca9
or hilely <or.h@ewave.co.il>
2020-03-26 11:29:30 +0200

    Initial Commit

---
+++ DiscountMovieAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiscountMovieAssignment
+//
+//  Created by or hilely on 26/03/2020.
+//  Copyright © 2020 or hilely. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
